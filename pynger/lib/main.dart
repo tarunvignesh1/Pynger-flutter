b969aae3dcc62269014d5426f5f06daee5c6cfe7
Tarun Vignesh Selvabalaji <tarunvignesh1@gmail.com>
2021-12-24 00:48:49 +0530

    learning

--- pynger/lib/main.dart
+++ pynger/lib/main.dart
@@ -1,9 +1,21 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @Override
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Explanation!',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
