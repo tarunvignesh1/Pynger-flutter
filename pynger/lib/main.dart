db68b693e48d06a0dda43a3b481f37fb6bbd9d6f
Tarun Vignesh Selvabalaji <tarunvignesh1@gmail.com>
2021-12-27 21:47:46 +0530

    adding

--- pynger/lib/main.dart
+++ pynger/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false, // removing debug banner
       home: Scaffold(
         appBar: AppBar(
           title: Text("I am a Billionare"),
