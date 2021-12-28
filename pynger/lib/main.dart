8e38d959f2cd6077d2d7744d51de17c8514b6fab
Tarun Vignesh Selvabalaji <tarunvignesh1@gmail.com>
2021-12-29 00:07:38 +0530

    adding icon

--- pynger/lib/main.dart
+++ pynger/lib/main.dart
@@ -11,8 +11,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fscottmendelson%2Ffiles%2F2018%2F02%2Fau_rich_hero_blackpanther_1_3c317c85-1200x526.jpg'),
+            image: AssetImage('images/diamond.jpg'),
           ),
         ),
         backgroundColor: Colors.lightBlue,
