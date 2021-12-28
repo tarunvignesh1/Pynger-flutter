8e995ffd163fa738f08f7de00c11dde5059ff5dc
Tarun Vignesh Selvabalaji <tarunvignesh1@gmail.com>
2021-12-28 23:44:09 +0530

    learning

--- pynger/lib/main.dart
+++ pynger/lib/main.dart
@@ -7,7 +7,15 @@ void main() {
       home: Scaffold(
         appBar: AppBar(
           title: Text("I am a Billionare"),
+          backgroundColor: Colors.lightBlue[900],
         ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fscottmendelson%2Ffiles%2F2018%2F02%2Fau_rich_hero_blackpanther_1_3c317c85-1200x526.jpg'),
+          ),
+        ),
+        backgroundColor: Colors.lightBlue,
       ),
     ),
   );
